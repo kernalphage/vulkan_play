eeb75e743f34ffa69a55ee6cea24361b58d90bcc
Matt Dobler <doblerman@gmail.com>
2018-01-04 09:49:27 -0800

    Chapter 4

--- main.cpp
+++ main.cpp
@@ -1,26 +1,49 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/mat4x4.hpp>
-#include <glm/vec4.hpp>
+
+#include <functional>
 #include <iostream>
+#include <stdexcept>
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+class HelloTriangleApplication {
+public:
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+private:
+  void initWindow() {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
+  void initVulkan() {}
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
+  void cleanup() {
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  }
+
+  GLFWwindow* window;
+};
 
 int main() {
-  glfwInit();
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow *window =
-      glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-  uint32_t extensionCount = 0;
-  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-  std::cout << extensionCount << " extensions supported" << std::endl;
-  glm::mat4 matrix;
-  glm::vec4 vec;
-  auto test = matrix * vec;
-  while (!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
+  HelloTriangleApplication app;
+  try {
+    app.run();
+  } catch (const std::runtime_error &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
   }
-  glfwDestroyWindow(window);
-  glfwTerminate();
-  return 0;
+  return EXIT_SUCCESS;
 }
