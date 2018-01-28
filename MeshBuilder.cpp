905360482747b2f122f8b7d9ee1d0f9a0596c1df
Matt Dobler <doblerman@gmail.com>
2018-01-28 13:50:50 -0800

    Doughnut time!

---
+++ MeshBuilder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matt on 1/28/18.
+//
+
+#include "MeshBuilder.hpp"
---
+++ MeshBuilder.hpp
@@ -0,0 +1,16 @@
+//
+// Created by matt on 1/28/18.
+//
+
+#ifndef VULKAN_PLAY_MESHBUILDER_HPP
+#define VULKAN_PLAY_MESHBUILDER_HPP
+
+
+
+class MeshBuilder {
+
+};
+
+
+
+#endif //VULKAN_PLAY_MESHBUILDER_HPP
